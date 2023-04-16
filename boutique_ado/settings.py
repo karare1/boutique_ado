7bd98b2fe1dd2983b7dba659130f4441a50c2436
karare1 <defi20@yahoo.co.uk>
2023-04-16 01:14:11 +0000

    completed home page header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -150,5 +150,9 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
