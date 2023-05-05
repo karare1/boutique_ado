f78463c4234fbd9e10373104a2d7b0ddf9cc7eb9
karare1 <defi20@yahoo.co.uk>
2023-05-05 21:56:14 +0000

    Added order summary to checkout success page.

--- checkout/views.py
+++ checkout/views.py
@@ -28,7 +28,9 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
+
         order_form = OrderForm(form_data)
+
         if order_form.is_valid():
             order = order_form.save()
             for item_id, item_data in bag.items():
